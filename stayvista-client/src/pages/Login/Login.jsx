5fc163e8d03cd7fa846b3c1657cc535b5e5685a5
Developer Ujjal <ujjalchandra213@gmail.com>
2025-01-05 15:06:58 +0600

    add login page func..

--- stayvista-client/src/pages/Login/Login.jsx
+++ stayvista-client/src/pages/Login/Login.jsx
@@ -1,7 +1,56 @@
-import { Link } from "react-router";
+import { Link, replace, useLocation, useNavigate } from "react-router";
 import { FcGoogle } from 'react-icons/fc'
+import useAuth from "../../hooks/useAuth";
+import toast from "react-hot-toast";
+import { ImSpinner9 } from "react-icons/im";
+import { useState } from "react";
 
 const Login = () => {
+  const { signIn, signInWithGoogle } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state || '/';
+
+
+  const handleSingIn = async (e) => {
+    e.preventDefault();
+
+    const form = new FormData(e.currentTarget);
+    const email = form.get('email');
+    const password = form.get('password');
+
+    try {
+      setIsLoading(true)
+      const userCredential = await signIn(email, password);
+      if (userCredential.user) {
+        toast.success('Successfully Logged In!');
+        navigate(from, replace);
+        setIsLoading(false)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
+
+  }
+
+
+
+  const handleGoogleSignIn = async () => {
+    try {
+      setIsLoading(true)
+      await signInWithGoogle();
+
+      navigate(from, replace)
+      toast.success('Successfully Logged In!')
+      setIsLoading(false)
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+
+
   return (
     <div className='flex justify-center items-center min-h-screen'>
       <div className='flex flex-col max-w-md p-6 rounded-md sm:p-10 bg-gray-100 text-gray-900'>
@@ -12,8 +61,7 @@ const Login = () => {
           </p>
         </div>
         <form
-          noValidate=''
-          action=''
+          onSubmit={handleSingIn}
           className='space-y-6 ng-untouched ng-pristine ng-valid'
         >
           <div className='space-y-4'>
@@ -54,12 +102,15 @@ const Login = () => {
               type='submit'
               className='bg-rose-500 w-full rounded-md py-3 text-white'
             >
-              Continue
+              {
+                isLoading ? <ImSpinner9 className="m-auto animate-spin" /> : 'Continue'
+              }
             </button>
           </div>
         </form>
         <div className='space-y-1'>
-          <button className='text-xs hover:underline hover:text-rose-500 text-gray-400'>
+          <button
+            className='text-xs hover:underline hover:text-rose-500 text-gray-400'>
             Forgot password?
           </button>
         </div>
@@ -70,11 +121,13 @@ const Login = () => {
           </p>
           <div className='flex-1 h-px sm:w-16 dark:bg-gray-700'></div>
         </div>
-        <div className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
+        <button
+          onClick={handleGoogleSignIn}
+          className='flex justify-center items-center space-x-2 border m-3 p-2 border-gray-300 border-rounded cursor-pointer'>
           <FcGoogle size={32} />
 
           <p>Continue with Google</p>
-        </div>
+        </button>
         <p className='px-6 text-sm text-center text-gray-400'>
           Don&apos;t have an account yet?{' '}
           <Link
--- stayvista-client/src/routes/PrivateRoute.jsx
+++ stayvista-client/src/routes/PrivateRoute.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types'
 import useAuth from '../hooks/useAuth'
-import { Navigate, useLocation } from 'react-router-dom'
 import LoadingSpinner from '../components/Shared/LoadingSpinner'
+import { Navigate, useLocation } from 'react-router'
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useAuth()
@@ -9,6 +9,7 @@ const PrivateRoute = ({ children }) => {
 
   if (loading) return <LoadingSpinner />
   if (user) return children
+
   return <Navigate to='/login' state={location.pathname} replace='true' />
 }
 
