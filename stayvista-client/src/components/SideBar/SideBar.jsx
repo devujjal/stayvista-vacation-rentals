88ea4fbf00c5e665fc5ab351d96671f2f84c92c1
Developer Ujjal <ujjalchandra213@gmail.com>
2025-01-09 23:32:54 +0600

    add dashboard toggle func..

--- stayvista-client/src/components/SideBar/SideBar.jsx
+++ stayvista-client/src/components/SideBar/SideBar.jsx
@@ -10,16 +10,23 @@ import MenuItem from '../Menu/MenuItem'
 import HostMenu from '../Menu/HostMenu'
 import GuestMenu from '../Menu/GuestMenu'
 import AdminMenu from '../Menu/AdminMenu'
+import ToggleBtn from '../DashboardToggleBtn/ToggleBtn'
 
 const SideBar = () => {
     const { logOut } = useAuth();
     const [isActive, setActive] = useState(false);
+    const [toggle, setToggle] = useState(true)
     const [role] = useRole();
 
     // Sidebar Responsive Handler
     const handleToggle = () => {
         setActive(!isActive)
     }
+
+    const toggleHandler = (e) => {
+        setToggle(e.target.checked)
+    }
+
 
     return (
         <>
@@ -70,6 +77,7 @@ const SideBar = () => {
                     {/* Nav Items */}
                     <div className='flex flex-col justify-between flex-1 mt-6'>
                         {/* Conditional toggle button here.. */}
+                        <ToggleBtn toggleHandler={toggleHandler} toggle={toggle} />
 
                         {/*  Menu Items */}
                         <nav>
@@ -77,7 +85,9 @@ const SideBar = () => {
                             <MenuItem label={'Statistics'} address={'/dashboard'} icon={BsGraphUp} />
 
                             {role?.role === 'guest' && <GuestMenu />}
-                            {role?.role === 'host' && <HostMenu />}
+
+                            {role?.role === 'host' ? toggle ? <HostMenu /> : <GuestMenu /> : undefined}
+
                             {role?.role === 'admin' && <AdminMenu />}
 
 
