f6e926840f05930ab96d4c763bd1eb2d2da1643b
Developer Ujjal <ujjalchandra213@gmail.com>
2025-01-10 23:08:10 +0600

    add my bookings page

--- stayvista-client/src/routes/Routes.jsx
+++ stayvista-client/src/routes/Routes.jsx
@@ -14,6 +14,7 @@ import Profile from '../pages/Dashboard/Profile/Profile'
 import ManageUsers from '../pages/Dashboard/ManageUsers/ManageUsers'
 import HostRoute from './HostRoute'
 import AdminRoute from './AdminRoute'
+import MyBookings from '../pages/Dashboard/MyBookings/MyBookings'
 
 export const router = createBrowserRouter([
   {
@@ -57,6 +58,10 @@ export const router = createBrowserRouter([
       {
         path: 'manage-users',
         element: <PrivateRoute><AdminRoute><ManageUsers /></AdminRoute></PrivateRoute>
+      },
+      {
+        path: 'my-bookings',
+        element: <PrivateRoute><MyBookings /></PrivateRoute>
       }
     ]
   },
