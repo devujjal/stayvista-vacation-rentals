2748b70927ecf8243bb6edb2b3aae812e7dd93eb
Developer Ujjal <ujjalchandra213@gmail.com>
2025-01-12 22:37:22 +0600

    add func for get client secret

--- stayvista-client/src/components/Form/CheckoutForm.jsx
+++ stayvista-client/src/components/Form/CheckoutForm.jsx
@@ -1,12 +1,35 @@
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
 import PropTypes from 'prop-types';
 import './CheckoutForm.css'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import toast from 'react-hot-toast'
+import useAxiosSecure from '../../hooks/useAxiosSecure';
 
-const CheckoutForm = ({ closeModal }) => {
+const CheckoutForm = ({ closeModal, totalPrice }) => {
     const stripe = useStripe();
     const elements = useElements();
+    const axiosSecure = useAxiosSecure();
     const [error, setError] = useState('');
+
+
+    useEffect(() => {
+        const getClientSecret = async () => {
+            if (!totalPrice || totalPrice < 1) {
+                return;
+            }
+
+            try {
+                const { data } = axiosSecure.post('/some', totalPrice);
+                console.log(data)
+
+            } catch (error) {
+                toast.error(error.message)
+            }
+        }
+
+
+    }, [axiosSecure, totalPrice])
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -58,7 +81,7 @@ const CheckoutForm = ({ closeModal }) => {
             <div className='flex mt-2 justify-around'>
                 <button
                     disabled={!stripe}
-                    type='button'
+                    type='submit'
                     className='inline-flex justify-center rounded-md border border-transparent bg-green-100 px-4 py-2 text-sm font-medium text-green-900 hover:bg-green-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-green-500 focus-visible:ring-offset-2'
                 >
                     Pay
@@ -80,7 +103,8 @@ const CheckoutForm = ({ closeModal }) => {
 };
 
 CheckoutForm.propTypes = {
-    closeModal: PropTypes.func
+    closeModal: PropTypes.func,
+    totalPrice: PropTypes.number
 }
 
 export default CheckoutForm;
